bd0910c4c2d279e86e36071ff148c92cd2496cfc
Aayush Sharma <aayushsh@umich.edu>
2025-05-09 00:26:58 -0400

    Added mobile support upto Tech Stack

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -66,7 +66,7 @@ const TechStack = () => {
   }, []);
 
   return (
-    <>
+    <div className="tech-stack-section">
       <div className="tech-stack-header">
         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
@@ -95,7 +95,7 @@ const TechStack = () => {
           ))}
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
